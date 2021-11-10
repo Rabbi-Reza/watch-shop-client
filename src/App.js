72cb7992158818b33fc18b1900ddb6d11d1a2f02
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2021-11-10 13:42:36 +0600

    Assignment 12 Firebase deployed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AuthProvider from './contexts/AuthProvider';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
